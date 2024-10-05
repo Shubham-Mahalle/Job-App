d2aa765075f1d89949e47e11955e9d6a1fef016a
Shubham-Mahalle <shubhammahalle691@gmail.com>
2024-10-05 19:23:41 +0530

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // Import the path module
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // Map '@' to the 'src' directory
+    },
+  },
+});
