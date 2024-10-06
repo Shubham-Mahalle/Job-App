1e226e2d085ffa4c58e681cce5bb7a84db1b4165
Shubham-Mahalle <150221292+Shubham-Mahalle@users.noreply.github.com>
2024-10-06 10:15:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,14 +23,10 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+
 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
