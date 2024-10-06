a650e74b3a983fea7dd20e3d867b70ce2efbb368
Shubham-Mahalle <150221292+Shubham-Mahalle@users.noreply.github.com>
2024-10-06 10:54:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,17 +16,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://career-connect-frontend-sepia.vercel.app',
+    origin:'',
     methods : ["POST","GET"],
     credentials:true
 }
 
 app.use(cors(corsOptions));
 
-
+const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application",applicationRoute);
 
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
