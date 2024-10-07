f5b444d89e3a60a019383a30bbeb16f57fe3281d
Shubham-Mahalle <150221292+Shubham-Mahalle@users.noreply.github.com>
2024-10-07 18:26:41 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/user";
-export const JOB_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/job";
-export const APPLICATION_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/application";
-export const COMPANY_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/company";
+export const Vite_USER_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/user";
+export const Vite_JOB_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/job";
+export const Vite_APPLICATION_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/application";
+export const Vite_COMPANY_API_END_POINT="https://career-connect-kappa.vercel.app/api/v1/company";
+
